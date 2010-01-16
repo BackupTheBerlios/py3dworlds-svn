6305474aa7c62a8a9bc2cb48e12ed83f7ee1b9eb
dav1d <dav1d@6ace8004-607c-0410-85f8-f4a0037e5e17>
2010-01-16 16:58:44 +0000

    ***

--- trunk/AssetServer.py
+++ trunk/AssetServer.py
@@ -20,19 +20,45 @@ class AssetServer(Resource,  basics, gridxml,  usefullThings):
         self.db_com = databases.DB_Com.DB_Com()
         #dicxml.__init__(self)
         usefullThings.__init__(self)
+        
+        self.handler = {}
                 
     def render_GET(self, request):
-        sRequest = request
-        print sRequest
+        print request
         print request.postpath
-        try:
-            if request.postpath == (".html"):
-                pass
-                
-        except IOError:
-            (404,'File Not Found: %s' % request.postpath)
-            
-        sRet =  "<html>Hello, world!  request = " + `sRequest`
-        sRet += " and postpath = " + `request.postpath`+ " </html>"
-
-        return sRet
+        #try:
+        #    if request.postpath == (".html"):
+        #        f = open(curdir + sep + self.path) #self.path has /test.html
+        #                                             
+        #except IOError:
+        #    (404,'File Not Found: %s' % request.postpath)
+        
+        pp = request.postpath
+        if pp[0] == 'assets':
+            if pp[1]:
+                self.getAsset(request,  pp[1])
+    
+        #return "<html>Hello, world!</html>"
+        
+    def getAsset(self,  request,  id):
+        sSQL = "SELECT data, name, assetType as type, local, temporary FROM assets WHERE id = '" + id + "'"
+        result = self.db_com.xmlrpc_executeNormalQuery(sSql)
+        
+        dic = {}
+        for row in result:
+            dic['Data'] = row['data']
+            dic['FullID'] = {'Guid' : id}
+            dic['ID'] = id
+            dic['name'] = row['name']
+            #dic['Description'] = None tag must be <Description/>
+            dic['Type'] = row['type']
+            dic['Local'] = row['local']
+            dic['Temporary'] = row['temporary']
+        
+        doc = self.createDoc(sDTD= self.dtd1, sRoot = 'AssetBase' )
+        sXml = self.dic2xml(doc, Folders)
+        #print sXml
+        
+        request.write(sXml)
+        request.finish()
+        
