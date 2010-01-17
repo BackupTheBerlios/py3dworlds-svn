61f3fa622f463bbc846d0ddd5e1e7c553ce302b9
dav1d <dav1d@6ace8004-607c-0410-85f8-f4a0037e5e17>
2010-01-17 13:32:30 +0000

    dicxml changed

--- trunk/xmlrpc/dicxml.py
+++ trunk/xmlrpc/dicxml.py
@@ -1,26 +1,76 @@
 # -*- coding: utf-8 -*-
 
 try:
-    from lxml import etree
+    from lxml import etree 
 except ImportError:
-    from lxml import cElementTree as etree
+    try:
+        from lxml import cElementTree as etree
+    except ImportError:
+        try:
+            import cElementTree as etree
+        except ImportError:
+            try:
+                import elementtree.ElementTree
+            except ImportError:
+                raise ImportError('etree is needed')
 
-class dicxml(etree):
-    def __init__(self):
-        pass
-        #etree.__init__(self)
-            
-    def parse_to_dict(xml):
-        root = etree.fromstring(xml)
+from time import clock
+
+def xml_to_dict(xml):
+    root = etree.fromstring(xml)
+    def parse(children):
         dic = {}
-        for child in root.getchildren():
+        for child in children:
             tag = child.tag
-            text = child.text
-            dic[tag] = text
+            if not child.getchildren():
+                text = child.text
+                dic[tag] = text
+            else:
+                dic[tag] = parse(iter(child.getchildren()))
         return dic
-        
-    def parse_to_xml(root,  dic):
-        xml = '<' + root + '>'
-        for key,  value in dic.iteritems():
-            xml += '<%s>%s</%s>' % (key,  value,  key)
-        xml += '</' + root + '>'
+    return root.tag, parse(iter(root.getchildren()))
+
+def dict_to_xml(base, root, dic):
+    def make(itr):
+        xml_part = ''
+        for key, value in itr:
+            if isinstance(value, dict):
+                xml_part += '<%s>%s</%s>' % (key,  make(iter(value.iteritems())), key)
+            elif value is None:
+                xml_part += '<%s/>' % (key)
+            else:
+                xml_part += '<%s>%s</%s>' % (key,  value,  key)
+        return xml_part
+    return '%s<%s>%s</%s>' % (base, root, make(iter(dic.iteritems())), root)
+
+
+if __name__ == '__main__':
+    x = '''<?xml version="1.0"?>
+    <InventoryFolderBase>
+        <Name>My Inventory</Name>
+        <ID>
+            <Guid>3e1a8134-f9d7-40a1-9a01-7fff99ac8536</Guid>
+        </ID>
+        <Owner>
+            <test>TEST!</test>
+            <Guid>fb65174f-2dde-4c1e-ba13-1a776d6864fd</Guid>
+        </Owner>
+        <ParentID>
+            <Guid>00000000-0000-0000-0000-000000000000</Guid>
+        </ParentID>
+        <Type>8</Type>
+        <Version>1</Version>
+    </InventoryFolderBase>'''
+    
+    start2dict = clock()
+    td = xml_to_dict(x)
+    end2dict = clock()
+    print td
+    start2xml = clock()
+    tx = dict_to_xml('<?xml version="1.0"?>', *td)
+    end2xml = clock()
+    print tx
+    #print '\n'.join([i.strip() for i in x.splitlines()])
+    print 'Zeit für xml_to_dict', end2dict - start2dict, 'Sekunden'
+    print 'Zeit für dict_to_xml', end2xml - start2xml, 'Sekunden'
+    print len(x.splitlines()), 'Zeilen'
